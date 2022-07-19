e78b5b1a0c132fb89cc13f685dc201a8724b39b6
thetechdude124 <aditya.dewan124@gmail.com>
2022-07-19 14:52:59 -0400

    --- Auto-Git Commit ---

--- DKPP_Distillation.py
+++ DKPP_Distillation.py
@@ -44,11 +44,43 @@ class DKPP_DistillationLoss(Function):
         first_grad_term = (1 / batch_size) * torch.sub(s_label_preds, t_label_preds)
         #Next, subtract the DK student logits by the permuted DK teacher logits (EXCEPT for the argmax dimension - do not modify) and normalize once again 
         # to obtain second gradient term
-        #Access the indexes of the first dimension (the index of the rows) and use .randperm to shuffle indexes
-        #Then, simply re-arrange the tensor with the new indexes to obtain the shuffled tensor
-        permuted_indexes = torch.randperm(t_smax_preds.shape[0])
-        permuted_DK_terms = t_smax_preds[permuted_indexes]
-        second_grad_term = (1 / batch_size) * torch.sub(s_smax_preds, permuted_DK_terms)
+        #In other words, shuffle the non-argmax logits of each sample
+        #First get the maximum values and indices
+        highest_pred, highest_pred_idxs = torch.max(t_smax_preds, dim = 1)
+        #Unsqueeze for row-wise comparisons
+        highest_pred.unsqueeze_(dim = 1)
+        highest_pred_idxs.unsqueeze_(dim = 1)
+        #Create a mask to remove all of the maximum values out of the tensor, leaving just logits that can be permuted
+        remove_max_mask = t_smax_preds.scatter_(1, highest_pred_idxs, 0)
+        teacher_nonmax_logits = t_smax_preds[remove_max_mask.bool()].view(batch_size, s_smax_preds.shape[1] - 1)
+        #Permute leftover logits
+        permuted_indexes = torch.randperm(teacher_nonmax_logits.shape[0])
+        permuted_logits = teacher_nonmax_logits[permuted_indexes]
+        #Append the argmax sample column back into the array (shape is now back bs * n_classes)
+        argmax_added_logits = torch.cat((permuted_logits, highest_pred), dim = 1)
+        #Create tensor of size (0, n_classes)
+        permuted_DK_logits = torch.zeros(0, argmax_added_logits.shape[1])
+        #Iterate over argmax_added_logits rows and place the argmax element back to its original position - concatenate each row to the above defined tensor
+        for (row_idx, row), (sample_number, max_val), (sample_number, max_index) in zip(enumerate(argmax_added_logits), enumerate(highest_pred), enumerate(highest_pred_idxs)):
+            #Get first part of the row (everything up to the original position of the argmax value), and concatenate with the argmax value
+            first_section = torch.cat((row[:max_index.item()].clone().detach(), max_val), dim = 0)
+            #As we appended all of the maximum values as a vector to the end of the logits matrix, the last element will always be the argmax one
+            #So, go from the max index to one short of the row's end to get the remaining part of the row
+            #Remember that the original row has not been modified - we are obtaining everything after and including the max index
+            second_section = row[max_index.item():row.shape[0]-1].clone().detach()
+            #Combine the two sections
+            new_row = torch.cat((first_section, second_section), dim = 0)
+            #Unsqueeze to turn into matrix and then add to permuted_DK_logits
+            new_row.unsqueeze_(dim = 0)
+            permuted_DK_logits = torch.cat((permuted_DK_logits, new_row), dim = 0)
+        #Now that we have the permuted DK logits, subtract all of the NON-ARGMAX values with torch.where
+        subtracted_logits = torch.where(permuted_DK_logits != highest_pred, s_smax_preds - permuted_DK_logits, permuted_DK_logits)
+        #Normalize to obtain second term
+        second_grad_term = (1 / batch_size) * subtracted_logits
+        #Element wise - for each element, if the value is the max value for that sample, leave it unchanged
+        #Otherwise randomly permute it (and the other non-argmax samples in that row) to serve as the logits for another sample
+        #permuted_DK_terms = torch.where(t_smax_preds != highest_pred, t_smax_preds[permuted_indexes], t_smax_preds)
+        #second_grad_term = (1 / batch_size) * torch.sub(s_smax_preds, permuted_DK_terms)
         #The first grad term is a vector of size bs, whereas the second grad term is of the size bs * n_classes - unsqueeze first term to (bs * 1)
         #As we must return bs * n_classes, add the vector to each column of the second grad term
         #Reverse order -> m + v instead of v + m
